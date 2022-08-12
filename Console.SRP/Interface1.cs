b48bf9a9724c37f9b6baa2ba827d8e66251b207a
support <support@WEMA-TRAINING-S>
2022-08-12 09:59:19 -0700

    Add project files.

---
+++ Console.SRP/Interface1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.SRP
+{
+    //OCP CONFIRMED
+    public interface IUploadOrderFile
+    {
+        object ProcessOrderFile();
+    }
+    public class UploadXMLOrderFile : IUploadOrderFile
+    {
+        public object ProcessOrderFile()
+        {
+            object orderObj = null;
+
+            //Parse XML File to DTO Object
+            return orderObj;
+        }
+    }
+
+    public class UploadPdfOrderFile : IUploadOrderFile
+    {
+        public object ProcessOrderFile()
+        {
+            object orderObj = null;
+            //Parse PDF File to DTO Object
+            return orderObj;
+        }
+    }
+    public class UploadProcess
+    {
+        public bool UploadFile()
+        {
+            IUploadOrderFile orderFile = null;
+            //if XML File
+            {
+                orderFile = new UploadXMLOrderFile();
+            }
+            //if PDF File
+            {
+                orderFile = new UploadPdfOrderFile();
+            }
+            Object orderObj = orderFile.ProcessOrderFile();
+            //Validate Records
+            //Save Records
+            return true;
+        }
+    }
+}
---
+++ Console.SRP/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Console.SRP
+{
+    //SRP CONFIRMED
+    public class OrderService
+    {
+        public string CreateOrder(string OrderDetails)
+        {
+            string OrderId = "";
+            //Code to Create Order
+            return OrderId;
+        }
+    }
+    public class PaymentService
+    {
+        public bool MakePayment(string PaymentDetails)
+        {
+            //Code to Make Payment
+            return true;
+        }
+    }
+    public class InvoiceService
+    {
+        public bool GenerateInvoice(string InvoiceDetails)
+        {
+            //Code to Generate Invoice
+            return true;
+        }
+    }
+    public class EmailService
+    {
+        public bool EmailInvoice(string EmailDetails)
+        {
+            //Code to Email Invoice
+            return true;
+        }
+    }
+    
+}
